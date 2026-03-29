792ea66f80572892674aef936eaae16c1a125df1
YiqiLanceZhang <YiqiLanceZhang@outlook.com>
2026-03-29 19:58:52 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Docker Pipeline! 3.1</h1>')
+})
+
+const PORT = 8080
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
